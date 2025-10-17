78a7335231a985e23ba8ec89c5f17d616dd80a60
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-17 12:13:52 -0400

    chore: init project

---
+++ src/test/java/com/raptor/virtuallibrary/VirtualLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raptor.virtuallibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

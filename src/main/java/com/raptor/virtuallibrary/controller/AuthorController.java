c1827bc998aa2f2c31b18811e379de7334645923
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-22 10:51:52 -0400

    feat(author): implement CRUD operations to Author Controller

--- src/main/java/com/raptor/virtuallibrary/controller/AuthorController.java
+++ src/main/java/com/raptor/virtuallibrary/controller/AuthorController.java
@@ -3,16 +3,15 @@ package com.raptor.virtuallibrary.controller;
 import com.raptor.virtuallibrary.entity.Author;
 import com.raptor.virtuallibrary.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/author")
+@RequestMapping("/api/authors")
 public class AuthorController {
 
     private final AuthorService authorService;
@@ -22,10 +21,38 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
+    // TODO: Aplicar Swagger
+
+    @GetMapping("/")
+    public ResponseEntity<List<Author>> findAll() {
+        return new ResponseEntity<>(authorService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Author> findById(@PathVariable int id) {
+        return new ResponseEntity<>(authorService.findById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Author> findByName(@PathVariable String name) {
+        return new ResponseEntity<>(authorService.findByName(name), HttpStatus.OK);
+    }
+
     @PostMapping("/")
     public ResponseEntity<Author> addAuthor(@RequestBody Author author) {
         var newAuthor = authorService.save(author);
         URI location =URI.create("/api/author/" + newAuthor.getId());
         return ResponseEntity.created(location).body(newAuthor);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Author> updateAuthor(@RequestBody Author author, @PathVariable Integer id) {
+        var newAuthor = authorService.save(author);
+        return ResponseEntity.ok(newAuthor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Author> deleteAuthor(@PathVariable Integer id) {
+        return ResponseEntity.ok().build();
     }
 }

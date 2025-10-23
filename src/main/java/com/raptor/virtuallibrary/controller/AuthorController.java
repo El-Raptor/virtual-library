ff3a408c57f2460709eff351dbdbb7e275b31046
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-23 15:11:03 -0400

    fix(author): fix ambiguous endpoint

--- src/main/java/com/raptor/virtuallibrary/controller/AuthorController.java
+++ src/main/java/com/raptor/virtuallibrary/controller/AuthorController.java
@@ -33,8 +33,8 @@ public class AuthorController {
         return new ResponseEntity<>(authorService.findById(id), HttpStatus.OK);
     }
 
-    @GetMapping("/{name}")
-    public ResponseEntity<Author> findByName(@PathVariable String name) {
+    @GetMapping
+    public ResponseEntity<Author> findByName(@RequestParam String name) {
         return new ResponseEntity<>(authorService.findByName(name), HttpStatus.OK);
     }
 

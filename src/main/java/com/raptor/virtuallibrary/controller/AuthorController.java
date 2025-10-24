f1feebc26d6d853b67f00d4aa07d5d5ee15bd4d9
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-10-24 19:46:41 -0400

    docs(author): add Swagger documentation on Author Controller

--- src/main/java/com/raptor/virtuallibrary/controller/AuthorController.java
+++ src/main/java/com/raptor/virtuallibrary/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.raptor.virtuallibrary.controller;
 import com.raptor.virtuallibrary.entity.Author;
 import com.raptor.virtuallibrary.service.AuthorService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -26,37 +27,139 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
-
+    @Operation(description = "Get all authors from database.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Authors found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            )
+    })
     @GetMapping("/")
     public ResponseEntity<List<Author>> findAll() {
         return new ResponseEntity<>(authorService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(description = "Get author by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Author found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Author not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            )
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<Author> findById(@PathVariable int id) {
-        return new ResponseEntity<>(authorService.findById(id), HttpStatus.OK);
+    public ResponseEntity<Author> findById(@Parameter(description = "Author's ID", required = true)
+                                           @PathVariable int id) {
+        var author = authorService.findById(id);
+        if (author == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(author, HttpStatus.OK);
     }
 
+    @Operation(description = "Get Author by its name")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Author found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Author not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            )
+    })
     @GetMapping
-    public ResponseEntity<Author> findByName(@RequestParam String name) {
-        return new ResponseEntity<>(authorService.findByName(name), HttpStatus.OK);
+    public ResponseEntity<Author> findByName(@Parameter(description = "Author Name") @RequestParam String name) {
+        var author = authorService.findByName(name);
+        if (author == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(author, HttpStatus.OK);
     }
 
+    @Operation(description = "Create new Author")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "New Author Created",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            )
+    })
     @PostMapping("/")
     public ResponseEntity<Author> addAuthor(@RequestBody Author author) {
         var newAuthor = authorService.save(author);
-        URI location =URI.create("/api/author/" + newAuthor.getId());
+        URI location = URI.create("/api/author/" + newAuthor.getId());
         return ResponseEntity.created(location).body(newAuthor);
     }
 
+    @Operation(description = "Update Author by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Author updated",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Author to be updated not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            )
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<Author> updateAuthor(@RequestBody Author author, @PathVariable Integer id) {
-        var newAuthor = authorService.save(author);
-        return ResponseEntity.ok(newAuthor);
+    public ResponseEntity<Author> updateAuthor(@RequestBody Author author,
+                                               @Parameter(description = "Author's ID to be updated")
+                                               @PathVariable Integer id) {
+        var existingAuthor = authorService.findById(id);
+        if (existingAuthor == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        author.setId(id);
+        var updatedAuthor = authorService.save(author);
+        return ResponseEntity.ok(updatedAuthor);
     }
 
+    @Operation(description = "Delete Author by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Author ID has been removed",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Author.class)
+                    )
+            )
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Author> deleteAuthor(@PathVariable Integer id) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
